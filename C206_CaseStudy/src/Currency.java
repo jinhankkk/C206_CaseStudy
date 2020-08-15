b43757ed8515e0a48e5ac52e9842a1ef22b8805c
19047287 <19047287@DESKTOP-436HIF5>
2020-08-15 18:39:33 +0800

    Charmain-Currency class

---
+++ C206_CaseStudy/src/Currency.java
@@ -0,0 +1,45 @@
+
+public class Currency {
+	
+	private String iso; 
+	private String currencyName;
+	private double buyRate;
+	private double sellRate;
+	
+	public Currency(String iso, String currencyName, double buyRate, double sellRate) {
+		this.iso = iso;
+		this.currencyName = currencyName;
+		this.buyRate = buyRate;
+		this.sellRate = sellRate;
+	}
+
+	public String getIso() {
+		return iso;
+	}
+
+	public String getCurrencyName() {
+		return currencyName;
+	}
+
+	public double getBuyRate() {
+		return buyRate;
+	}
+
+	public double getSellRate() {
+		return sellRate;
+	}
+	
+	public String toString()
+	{
+		String iso = this.iso;
+		String currencyName = this.currencyName;
+		String buyRate = Double.toString(this.buyRate);
+		String sellRate = Double.toString(this.sellRate);
+		
+		return iso + ", " + currencyName + ", " + buyRate + ", " + sellRate ;
+		
+	}
+	
+	
+
+}
