880dc9c266eda324f00a135db574eeba03f658df
18046748 <18046748@LAPTOP-9SV4Q2MR.lan>
2020-08-15 18:43:32 +0800

    ok

--- C206_CaseStudy/src/MoneyHolding.java
+++ C206_CaseStudy/src/MoneyHolding.java
@@ -1,4 +1,24 @@
 
 public class MoneyHolding {
 
+	private String iso;
+	private double holdingAmt;
+	
+	public MoneyHolding(String iso, double holdingAmt) {
+		this.iso = iso;
+		this.holdingAmt = holdingAmt;
+	}
+	
+	public String getIso() {
+		return iso;
+	}
+	
+	public double getHoldingAmt() {
+		return holdingAmt;
+	}
+	
+	public String toString() {
+		return ;
+	}
+	
 }
