e791e5b01b646c97f60e8669b1caea4e924b077a
19021263 <19021263@DESKTOP-NT0IO4H>
2020-08-18 17:46:41 +0800

    add & delete currency

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -9,47 +9,50 @@ public class C206_CaseStudyTest {
 
 	private Currency cc1;
 	private Currency cc2;
-	
+
 	private MoneyHolding mh1;
 	private MoneyHolding mh2;
-	
+
 	private Transaction t1;
 	private Transaction t2;
-	
 
 	private ArrayList<Currency> currencyList;
 	private ArrayList<MoneyHolding> holdingList;
 	private ArrayList<Transaction> transactionList;
+
 	@Before
-	public void setUp() throws Exception
-	{
-		cc1 = new Currency("MYR","Malaysia Ringt",3.075,3.070);
-		cc2 = new Currency("KRW","Korean Won",878.73,874.89);
+	public void setUp() throws Exception {
+		cc1 = new Currency("MYR", "Malaysia Ringt", 3.075, 3.070);
+		cc2 = new Currency("KRW", "Korean Won", 878.73, 874.89);
 		mh1 = new MoneyHolding("USD", 100000);
 		mh2 = new MoneyHolding("MYR", 500000);
-		
 
 		currencyList = new ArrayList<>();
+		currencyList.add(cc1);
+		currencyList.add(cc2);
 		
 		holdingList = new ArrayList<>();
-		
-		transactionList = new ArrayList<>();
-		
-		
-	}
-	public void searchRateByCurrency()
-	{
-		
-		
-	}
-	
-	@Test
-	public void addMoneyHoldingTest() {
 		holdingList.add(mh1);
 		holdingList.add(mh2);
+		
+		transactionList = new ArrayList<>();
+
+	}
+
+	public void searchRateByCurrency() {
+
+	}
+
+	@Test
+	public void addMoneyHoldingTest() {
 		assertNotNull("Check the holding not empty.", holdingList);
-		
-		
+	}
+
+	public void addCurrencyTest() {
+		assertNotNull("Check the currency not empty.", currencyList);
+	}
+	public void deleteCurrencyTest() {
+		assertNotNull("Check the currency exist.", currencyList);
 	}
 
 }
