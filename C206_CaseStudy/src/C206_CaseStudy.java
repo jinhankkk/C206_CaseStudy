9fbc5541ef782c5b365b505aa1fd0482112564d8
19034471 <19034471@KKK>
2020-08-18 21:20:10 +0800

    11111111111

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -56,7 +56,8 @@ public class C206_CaseStudy {
 			}  else if (option == 10) {
 				viewAllTransaction(transactionList);
 			}  else if (option == 11) {
-				deleteTransaction(transactionList, 1);
+				int id1=Helper.readInt("Enter the transaction ID of the record:");
+				deleteTransaction(transactionList, id1);
 			} else if (option == 12) {
 				searchHoldingAndSgdValue(holdingList,currencyList);
 			} else if  (option == 13){
@@ -465,7 +466,7 @@ public class C206_CaseStudy {
 	}
 	
 	public static void deleteTransaction(ArrayList<Transaction>transactionList,int id) {
-		id=Helper.readInt("Enter the transaction ID of the record:");
+		
         for(int i =0; i<transactionList.size(); i++) {
                if(transactionList.get(i).getTrid()==id) {
                 transactionList.remove(i);
