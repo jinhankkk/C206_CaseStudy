588ecbb5412d66fe97d6a6fe842fd7795986580d
19046242 <19046242@DESKTOP-J6TS7B9>
2020-08-18 17:26:34 +0800

    done

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -2,6 +2,9 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class C206_CaseStudyTest {
 
 	private Currency cc1;
@@ -14,23 +17,38 @@ public class C206_CaseStudyTest {
 	private Transaction t2;
 	
 
-	ArrayList<Currency> currencyList;
-	ArrayList<MoneyHolding> holdingList;
-	ArrayList<Transaction> transactionList;
-	
+	private ArrayList<Currency> currencyList;
+	private ArrayList<MoneyHolding> holdingList;
+	private ArrayList<Transaction> transactionList;
+	@Before
 	public void setUp() throws Exception
 	{
 		cc1 = new Currency("MYR","Malaysia Ringt",3.075,3.070);
 		cc2 = new Currency("KRW","Korean Won",878.73,874.89);
+		mh1 = new MoneyHolding("USD", 100000);
+		mh2 = new MoneyHolding("MYR", 500000);
+		
 
 		currencyList = new ArrayList<>();
+		
 		holdingList = new ArrayList<>();
+		
 		transactionList = new ArrayList<>();
 		
 		
 	}
 	public void searchRateByCurrency()
 	{
+		
+		
+	}
+	
+	@Test
+	public void addMoneyHoldingTest() {
+		holdingList.add(mh1);
+		holdingList.add(mh2);
+		assertNotNull("Check the holding not empty.", holdingList);
+		
 		
 	}
 
